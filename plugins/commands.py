47ff530c050b3d63eb55a19668801050079c3b0e
santhoshkbd07 <154353388+santhoshkbd07@users.noreply.github.com>
2023-12-19 21:34:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -844,7 +844,7 @@ async def shortlink(bot, message):
     try:
         command, shortlink_url, api = data.split(" ")
     except:
-        return await message.reply_text("<b>Command Incomplete :(\n\nGive me a shortlink and api along with the command !\n\nFormat: <code>/shortlink shorturllink.in 7a8195c40d31e0c3b6baa6813fcecb123f29 ur copied API </code></b>")
+        return await message.reply_text("<b>Command Incomplete :(\n\nGive me a shortlink and api along with the command !\n\nFormat: <code>/shortlink shorturllink.in 0a46a267491e73ad689392c633af47c3c1b00a43 ur copied API </code></b>")
     reply = await message.reply_text("<b>Please Wait...</b>")
     await save_group_settings(grpid, 'shortlink', shortlink_url)
     await save_group_settings(grpid, 'shortlink_api', api)
